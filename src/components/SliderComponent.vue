7a1e0aa1c59f9d5a8c90ea77bffc45b53e2183fa
bhayukresnap <bhayukresnapangestoe@gmail.com>
2021-12-23 22:28:33 +0700

    .

--- src/components/SliderComponent.vue
+++ src/components/SliderComponent.vue
@@ -31,33 +31,4 @@ h2 {
   overflow: auto;
   width: 100%;
 }
-.animeCard {
-  position: relative;
-  width: 175px;
-}
-.animeTitle {
-  display: inline-block;
-  text-overflow: ellipsis;
-  overflow: hidden;
-  white-space: nowrap;
-  width: 100%;
-  font-family: Biotif-bold;
-  margin: 0.5em auto;
-}
-.badgeType {
-  position: absolute;
-  top: 10px;
-  left: 0;
-  font-family: Biotif-bold;
-  font-size: 12px;
-  z-index: 1;
-}
-.badgeScore {
-  position: absolute;
-  font-size: 14px;
-  top: 10px;
-  right: 0;
-  z-index: 1;
-  font-family: Biotif-bold;
-}
 </style>
